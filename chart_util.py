934dd73fcf5b32d6307737d9348db4ff1b990d44
Hoang Minh Chau <kidphys@gmail.com>
2025-11-20 13:52:22 +0700

    Refactor chart out of dashboard

--- chart_util.py
+++ chart_util.py
@@ -1,3 +1,6 @@
+import altair as alt
+
+
 def map_data_with_audience_category(selected_slide, df):
     slide_type = df[df['Slideid'] == selected_slide['Slideid']]['Slidetypenormalized'].iloc[0]
     if slide_type == 'Poll':
@@ -14,3 +17,36 @@ def map_data_with_audience_category(selected_slide, df):
         data = df.merge(audience_df, on='Audience Name', how='left')
         data.rename(columns={'Correct_y': 'Category'}, inplace=True)
         return data
+
+
+
+def create_segment_line_chart(data, y_field='Interaction Count', title='Empty', type='number'):
+    if type == 'percent':
+        y_field_tool_tip = alt.Tooltip(f'{y_field}:Q', format='.2~%')
+    elif type == 'number':
+        y_field_tool_tip = alt.Tooltip(f'{y_field}:Q', format='.2~f')
+    else:
+        raise ValueError(f'Invalid type: {type}')
+
+    chart = alt.Chart(data).mark_line(
+            size=2,
+            point=alt.OverlayMarkDef(filled=True, size=80)
+            ).encode(
+        x=alt.X('# Slidetitle:N', title='Slide Title',
+                sort=alt.EncodingSortField(field='Slideorder', op='min', order='ascending'),
+                axis=alt.Axis(ticks=True, tickBand='center', labelAngle=-45),   # Rotate x-axis labels to make them easier to read
+        ),
+        y=f'{y_field}:Q',  # count of interactions as the y-axis
+        color='Segment:N',
+        tooltip=['Segment:N', y_field_tool_tip, 'Slidetitle:N', alt.Tooltip('Slidetypenormalized:N', title='Slide Type')]
+    ).properties(
+        title=title
+    )
+        # Display technical options on chart
+    chart = chart.copy()
+    chart["usermeta"] = {
+        "embedOptions": {
+            "actions": {"export": True, "source": False, "compiled": False, "editor": False}
+        }
+    }
+    return chart
